e7c8e4ca875d1fda1896a2ea523c024023281d86
Adrien Padol <adrien.padol@orange.com>
2020-06-19 17:27:15 +0200

    Added a working implementation of the WorkManager synchronisation service

--- PeriodicBackgroundService/PeriodicBackgroundService.Android/MainActivity.cs
+++ PeriodicBackgroundService/PeriodicBackgroundService.Android/MainActivity.cs
@@ -3,45 +3,66 @@ using Android.App;
 using Android.Widget;
 using Android.OS;
 using Android.Content;
+using AndroidX.Work;
+using System;
 
 namespace PeriodicBackgroundService.Android
 {
-	[Activity(Label = "PeriodicBackgroundService", MainLauncher = true, Icon = "@mipmap/icon")]
-	public class MainActivity : Activity
-	{
-		int count = 1;
+    [Activity(Label = "PeriodicBackgroundService", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
 
-		protected override void OnCreate(Bundle savedInstanceState)
-		{
-			base.OnCreate(savedInstanceState);
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
 
-			// Set our view from the "main" layout resource
-			SetContentView(Resource.Layout.Main);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
 
-			// Get our button from the layout resource,
-			// and attach an event to it
-			Button button = FindViewById<Button>(Resource.Id.myButton);
+            // Get our button from the layout resource,
+            // and attach an event to it
+            Button button = FindViewById<Button>(Resource.Id.myButton);
 
-			button.Click += delegate
-			{
-				button.Text = string.Format("{0} clicks!", count++);
-			};
+            button.Click += delegate
+            {
+                button.Text = string.Format("{0} clicks!", count++);
+            };
 
-			SetAlarmForBackgroundServices(this);
-		}
+            //SetAlarmForBackgroundServices(this);
+            this.StartBackgroundServices();
+        }
 
-		public static void SetAlarmForBackgroundServices(Context context)
-		{
-			var alarmIntent = new Intent(context.ApplicationContext, typeof(AlarmReceiver));
-			var broadcast = PendingIntent.GetBroadcast(context.ApplicationContext, 0, alarmIntent, PendingIntentFlags.NoCreate);
-			if (broadcast == null)
-			{
-				var pendingIntent = PendingIntent.GetBroadcast(context.ApplicationContext, 0, alarmIntent, 0);
-				var alarmManager = (AlarmManager)context.GetSystemService(Context.AlarmService);
-				alarmManager.SetRepeating(AlarmType.ElapsedRealtimeWakeup, 300, 10000, pendingIntent);
-			}
-		}
-	}
+        private void StartBackgroundServices()
+        {
+            var constraints = new Constraints.Builder()
+                        .SetRequiredNetworkType(NetworkType.Connected)
+                        .Build();
+
+            // Cannot set a period inferior to 15 minutes
+            // source: https://medium.com/@yonatanvlevin/the-minimum-interval-for-periodicwork-is-15-minutes-same-as-jobscheduler-periodic-job-eb2d63716d1f#:~:text=The%20minimum%20interval%20for%20PeriodicWork,same%20as%20JobScheduler%20Periodic%20Job).
+            var periodicWork = PeriodicWorkRequest.Builder
+                .From<SyncDataWorker>(TimeSpan.FromMinutes(15))
+                .SetConstraints(constraints)
+                .Build();
+
+            WorkManager
+                .GetInstance(this.ApplicationContext)
+                .Enqueue(periodicWork);
+        }
+
+        //public static void SetAlarmForBackgroundServices(Context context)
+        //{
+        //	var alarmIntent = new Intent(context.ApplicationContext, typeof(AlarmReceiver));
+        //	var broadcast = PendingIntent.GetBroadcast(context.ApplicationContext, 0, alarmIntent, PendingIntentFlags.NoCreate);
+        //	if (broadcast == null)
+        //	{
+        //		var pendingIntent = PendingIntent.GetBroadcast(context.ApplicationContext, 0, alarmIntent, 0);
+        //		var alarmManager = (AlarmManager)context.GetSystemService(Context.AlarmService);
+        //		alarmManager.SetRepeating(AlarmType.ElapsedRealtimeWakeup, 300, 10000, pendingIntent);
+        //	}
+        //}
+    }
 }
 
 
---
+++ PeriodicBackgroundService/PeriodicBackgroundService.Android/SyncDataWorker.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using Android.Content;
+using Android.Runtime;
+using Android.Util;
+using AndroidX.Work;
+
+namespace PeriodicBackgroundService.Android
+{
+    /// <summary>
+    /// Doc: https://devblogs.microsoft.com/xamarin/getting-started-workmanager/
+    /// </summary>
+    public class SyncDataWorker : Worker
+    {
+        static readonly HttpClient client = new HttpClient();
+
+        public SyncDataWorker(IntPtr javaReference, JniHandleOwnership transfer) : base(javaReference, transfer)
+        {
+        }
+
+        public SyncDataWorker(Context context, WorkerParameters workerParams) : base(context, workerParams)
+        {
+        }
+
+        public override Result DoWork()
+        {
+            string path = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+            string fileName = Path.Combine(path, "backgroundfile.txt");
+            Console.WriteLine(fileName);
+
+            Log.Info("SyncDataWorker", "Start");
+
+            using (var streamWriter = new StreamWriter(fileName, true))
+            {
+                streamWriter.WriteLine("[SyncDataWorker] Begin task at " + DateTime.Now);
+
+                try
+                {
+                    try
+                    {
+                        streamWriter.WriteLine("[SyncDataWorker] Calling https://jsonplaceholder.typicode.com/todos/1 at " + DateTime.Now);
+
+                        var getDataTask = client.GetAsync("https://jsonplaceholder.typicode.com/todos/1");
+                        getDataTask.Wait();
+                        HttpResponseMessage response = getDataTask.Result;
+
+                        response.EnsureSuccessStatusCode();
+
+                        var readFileTask = response.Content.ReadAsStringAsync();
+                        readFileTask.Wait();
+                        string responseBody = readFileTask.Result;
+
+                        streamWriter.WriteLine("[SyncDataWorker] API response : " + responseBody);
+
+                    }
+                    catch (HttpRequestException e)
+                    {
+                        streamWriter.WriteLine("[SyncDataWorker] An error occured while calling API at " + DateTime.Now);
+                    }
+                    streamWriter.WriteLine("[SyncDataWorker] Task ended at " + DateTime.Now);
+                    streamWriter.WriteLine("");
+
+                    Log.Info("SyncDataWorker", "End");
+
+                    return Result.InvokeSuccess();
+                }
+                catch (Exception ex)
+                {
+                    streamWriter.WriteLine("[SyncDataWorker] Exception task at " + DateTime.Now);
+                    streamWriter.WriteLine(ex);
+
+                    Log.Info("SyncDataWorker", "Fail");
+
+                    return Result.InvokeFailure();
+                }
+            }
+        }
+    }
+}
